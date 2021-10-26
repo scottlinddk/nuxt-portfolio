fed8b150f61d3a1c59db074fde157dc24faa9b7f
Scott Lind <scttlnd@gmail.com>
2021-10-26 16:11:56 +0200

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  target: 'static',
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
