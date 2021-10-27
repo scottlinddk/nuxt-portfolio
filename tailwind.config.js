e8cf4e235ba3bd55239dc0b001c2e3d89a7cac7a
Scott Lind <scttlnd@gmail.com>
2021-10-27 08:47:20 +0200

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,29 @@
 module.exports = {
   purge: [
-    "./components/**/*.{vue,js}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}"
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue'
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
-      colors: {
-        green: {
-          350: "#2E5E4E",
-          450: "#0D2327",
-          950: "#001E26",
-        },
-        gray: {
-          450: "#93A7AA"
-        }
-      },
       animation: {
-        tilt: "tilt 10s infinite linear"
+        tilt: 'tilt 10s infinite linear'
       },
       keyframes: {
         tilt: {
-          "0%, 50%, 100%": {
-            transform: "rotate(0deg)"
+          '0%, 50%, 100%': {
+            transform: 'rotate(0deg)'
           },
-          "25%": {
-            transform: "rotate(1deg)"
+          '25%': {
+            transform: 'rotate(1deg)'
           },
-          "75%": {
-            transform: "rotate(-1deg)"
+          '75%': {
+            transform: 'rotate(-1deg)'
           }
         }
       }
     }
   },
-  variants: {
-    extend: {}
-  },
-  plugins: [require("tailwindcss"), require("autoprefixer")]
+  // plugins: [require('tailwindcss'), require('autoprefixer')]
 }
