535636c107bdc5b69ecd921f2c9e3d458ad7ad26
Scott Lind <scttlnd@gmail.com>
2021-11-08 14:32:47 +0100

    running

--- components/Runs.vue
+++ components/Runs.vue
@@ -1,34 +1,43 @@
 <template>
-<section v-if="dataReady" id="runs">
-  <div v-html="lastRun.content.rendered"></div>
-</section>
+  <section v-if="dataReady" id="runs">
+    <h2
+      class="
+        text-gray-900 text-center
+        mb-6
+        text-2xl
+        font-bold
+        tracking-tight
+        sm:text-4xl
+      "
+    >
+      Jeg kan gode lide at 🏃🏼
+    </h2>
+    <div class="mx-auto">
+      <div class="max-w-prose mx-auto" v-html="lastRun.content.rendered"></div>
+    </div>
+  </section>
 </template>
 
 <script>
-
 export default {
-  name: 'Runs',
+  name: "Runs",
   data() {
     return {
       dataReady: false,
-      runsAPI: 'https://wp.scottlind.dk/wp-json/wp/v2/posts?categories=3',
+      runsAPI: "https://wp.scottlind.dk/wp-json/wp/v2/posts?categories=3",
       errors: [],
-      lastRun: null
+      lastRun: null,
     };
   },
-    async created () {
+  async created() {
     try {
       const response = await this.$axios.get(this.runsAPI);
       this.lastRun = await response.data[0];
-      console.log(response)
       this.dataReady = true;
-      // const response = await this.$axios.get(this.menuAPI)
-      // this.menu = await response.data.items
-      // this.dataReady = true
     } catch (e) {
-      this.errors.push(e)
+      this.errors.push(e);
     }
-  }
-}
+  },
+};
 </script>
 
